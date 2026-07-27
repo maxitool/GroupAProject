75f2abfcd1cfefdfe444e89bdae766fbb7c1bd25
hutatBoosty <Parfenov.max24@yandex.ru>
2026-07-27 15:39:13 +0700

    Create CarValidator
    Create CarValidator

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.time.Year;
 import java.util.Objects;
 
 public class Car {
@@ -9,68 +8,37 @@ public class Car {
     }
 
     private static final int COUNT_FIELDS = 3;
-    private boolean isValidationGood;
     private int horsepower;
     private String model;
     private int year;
 
     public Car() {
-        isValidationGood = false;
         horsepower = -1;
         model = "";
         year = -1;
     }
     public Car(int horsepower, String model, int year) {
         this();
-        isValidationGood = true;
-        setHorsepower(horsepower, false).setModel(model, false).setYear(year, false);
+        setHorsepower(horsepower).setModel(model).setYear(year);
     }
 
-    public boolean getIsValidationGood() { return isValidationGood; }
     public int getHorsepower() { return horsepower; }
     public String getModel() { return model; }
     public int getYear() { return year; }
 
-    private Car setHorsepower(int horsepower, boolean doAnotherValidations) {
-        if (!doValidationHorsepower(true, horsepower)) return this;
+    private Car setHorsepower(int horsepower) {
         this.horsepower = horsepower;
-        if (doAnotherValidations) {
-            isValidationGood = true;
-            doValidationModel(false, model);
-            doValidationYear(false, year);
-        }
         return this;
     }
-    public Car setHorsepower(int horsepower) {
-        return setHorsepower(horsepower, true);
-    }
 
-    private Car setModel(String model, boolean doAnotherValidations) {
-        if (!doValidationModel(true, model)) return this;
+    private Car setModel(String model) {
         this.model = model;
-        if (doAnotherValidations) {
-            isValidationGood = true;
-            doValidationHorsepower(false, horsepower);
-            doValidationYear(false, year);
-        }
         return this;
-    }
-    public Car setModel(String model) {
-        return  setModel(model, true);
     }
 
-    private Car setYear(int year, boolean doAnotherValidations) {
-        if (!doValidationYear(true, year)) return this;
+    private Car setYear(int year) {
         this.year = year;
-        if (doAnotherValidations) {
-            isValidationGood = true;
-            doValidationHorsepower(false, horsepower);
-            doValidationModel(false, model);
-        }
         return this;
-    }
-    public Car setYear(int year) {
-        return setYear(year, true);
     }
 
     public static Car stringToCar(String data) {
@@ -93,36 +61,6 @@ public class Car {
             System.out.println("Can't convert String data ( " + data + " ) to Car class, reason: " + e.getMessage());
         }
         return null;
-    }
-
-    private boolean doValidationHorsepower(boolean isPrintInfoToConsole, int horsepower) {
-        if (horsepower < 0) {
-            if (isPrintInfoToConsole) System.out.println("The horsepower of car must be greater than or equal to 0. The horsepower: " + horsepower);
-            isValidationGood = false;
-            return false;
-        }
-        return true;
-    }
-    private boolean doValidationModel(boolean isPrintInfoToConsole, String model) {
-        if (model == null || model.isEmpty()) {
-            if (isPrintInfoToConsole) System.out.println("The model of car is null or empty");
-            isValidationGood = false;
-            return false;
-        }
-        return true;
-    }
-    private boolean doValidationYear(boolean isPrintInfoToConsole, int year) {
-        if (year < 0) {
-            if (isPrintInfoToConsole) System.out.println("The year of car must be greater than or equal to 0. The year: " + year);
-            isValidationGood = false;
-            return false;
-        }
-        if (year > Year.now().getValue()) {
-            if (isPrintInfoToConsole) System.out.println("The year of car must be less than or equal to current year ( " + Year.now().getValue() + " ). The year: " + year);
-            isValidationGood = false;
-            return false;
-        }
-        return true;
     }
 
     @Override
---
+++ src/main/java/org/example/CarValidator.java
@@ -0,0 +1,47 @@
+package org.example;
+
+import java.time.Year;
+
+public class CarValidator {
+    public static boolean validateCar(Car car, boolean isPrintInfoToConsole) {
+        if (car == null) {
+            if (isPrintInfoToConsole) System.out.println("Car class is null.");
+            return false;
+        }
+        return validateHorsepower(car.getHorsepower(), isPrintInfoToConsole) &&
+                validateModel(car.getModel(), isPrintInfoToConsole) &&
+                validateYear(car.getYear(), isPrintInfoToConsole);
+    }
+    public static boolean validateCar(Car car) { return validateCar(car, true); }
+
+    public static boolean validateHorsepower(int horsepower, boolean isPrintInfoToConsole) {
+        if (horsepower < 0) {
+            if (isPrintInfoToConsole) System.out.println("The horsepower of car must be greater than or equal to 0. The horsepower: " + horsepower);
+            return false;
+        }
+        return true;
+    }
+    public static boolean validateHorsepower(int horsepower) { return validateHorsepower(horsepower, true); }
+
+    public static boolean validateModel(String model, boolean isPrintInfoToConsole) {
+        if (model == null || model.isEmpty()) {
+            if (isPrintInfoToConsole) System.out.println("The model of car is null or empty");
+            return false;
+        }
+        return true;
+    }
+    public static boolean validateModel(String model) { return validateModel(model, true); }
+
+    public static boolean validateYear(int year, boolean isPrintInfoToConsole) {
+        if (year < 0) {
+            if (isPrintInfoToConsole) System.out.println("The year of car must be greater than or equal to 0. The year: " + year);
+            return false;
+        }
+        if (year > Year.now().getValue()) {
+            if (isPrintInfoToConsole) System.out.println("The year of car must be less than or equal to current year ( " + Year.now().getValue() + " ). The year: " + year);
+            return false;
+        }
+        return true;
+    }
+    public static boolean validateYear(int year) { return validateYear(year, true); }
+}
