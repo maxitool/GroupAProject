946891ec2a6e5ace7ae669d85945800392ffeb39
zotsss666 <zotsss666@gmail.com>
2026-07-28 02:05:59 +0500

    add toString in CustomArrayList

--- src/main/java/org/example/collections/CustomArrayList.java
+++ src/main/java/org/example/collections/CustomArrayList.java
@@ -67,4 +67,16 @@ public class CustomArrayList<T> implements Iterable<T> {
             }
         };
     }
+
+    @Override
+    public String toString(){
+        if (size == 0) return "[]";
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < size; i++){
+            sb.append(elements[i]);
+            if (i < size - 1) sb.append(", ");
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
