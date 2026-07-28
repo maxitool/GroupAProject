db4e09ce99a513a4768d7c032d29fd3dd2aaf331
hutatBoosty <Parfenov.max24@yandex.ru>
2026-07-28 13:26:48 +0700

    Change CarDeserializer.stringToCar function

--- src/main/java/org/example/CarDeserializer.java
+++ src/main/java/org/example/CarDeserializer.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class CarDeserializer {
     public static Car stringToCar(String data) {
         if (data == null) {
@@ -8,15 +10,33 @@ public class CarDeserializer {
         }
         String dataWithoutCar = data.replace("Car", "").replace("{","").replace("}","").trim();
         String[] dataArray = dataWithoutCar.split(",");
-        if (dataArray.length != Car.COUNT_FIELDS) {
-            System.out.println("Count fields in Car class must be " + Car.COUNT_FIELDS + ", the received data have " + dataArray.length + " fields");
+        HashMap<String, String> dataHashMap = new HashMap<>();
+        String[] keyValue;
+        for (int i = 0; i < dataArray.length; i++) {
+            keyValue = dataArray[i].split("=");
+            if (keyValue.length != 2) {
+                System.out.println(i + " argument of " + data +  " car class incorrectly set, correct format: key = value");
+                return null;
+            }
+            dataHashMap.put(keyValue[0], keyValue[1]);
+        }
+        if (!dataHashMap.containsKey("horsepower")) {
+            System.out.println("Didn't receive horsepower field for " + data + " Car class");
+            return null;
+        }
+        if (!dataHashMap.containsKey("model")) {
+            System.out.println("Didn't receive model field for " + data + " Car class");
+            return null;
+        }
+        if (!dataHashMap.containsKey("year")) {
+            System.out.println("Didn't receive year field for " + data + " Car class");
             return null;
         }
         try {
             return new Car(
-                    Integer.parseInt(dataArray[0].replace("horsepower","").replace("=","").replace("hp","").trim()),
-                    dataArray[1].replace("model","").replace("=","").replace("'","").trim(),
-                    Integer.parseInt(dataArray[2].replace("year","").replace("=","").trim()));
+                    Integer.parseInt(dataHashMap.get("horsepower").replace("hp","").trim()),
+                    dataHashMap.get("model").replace("'","").trim(),
+                    Integer.parseInt(dataHashMap.get("year").trim()));
         } catch (NumberFormatException e) {
             System.out.println("Can't convert String data ( " + data + " ) to Car class, reason: " + e.getMessage());
         }
