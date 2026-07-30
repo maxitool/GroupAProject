b07fcd47966ba02fdf2e24c8711697360dfbd5d2
hutatBoosty <Parfenov.max24@yandex.ru>
2026-07-30 16:20:13 +0700

    Add CarTest and CarDeserializerTest

---
+++ src/test/java/CarDeserializerTest.java
@@ -0,0 +1,100 @@
+import org.example.Car;
+import org.example.CarDeserializer;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CarDeserializerTest {
+
+    @Test
+    public void stringToCar_withGoodData_car() {
+        String stringCar1 = "Car{horsepower=290 hp, model='Chrysler PYR', year=1974}";
+        Car car = new Car(290, "Chrysler PYR", 1974);
+        Assertions.assertEquals(car, CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withGoodDataWithoutCarShell_car() {
+        String stringCar1 = "horsepower=290 hp, model='Chrysler PYR', year=1974";
+        Car car = new Car(290, "Chrysler PYR", 1974);
+        Assertions.assertEquals(car, CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withGoodDataWithoutSpaces_car() {
+        String stringCar1 = "Car{horsepower=290 hp,model='Chrysler PYR',year=1974}";
+        Car car = new Car(290, "Chrysler PYR", 1974);
+        Assertions.assertEquals(car, CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withMoreKeys_car() {
+        String stringCar1 = "Car{horsepower=290 hp, model='Chrysler PYR', year=1974, key1=1, key2='value'}";
+        Car car = new Car(290, "Chrysler PYR", 1974);
+        Assertions.assertEquals(car, CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withGoodDataWithoutHpForHorsepower_car() {
+        String stringCar1 = "Car{horsepower=290,model='Chrysler PYR',year=1974}";
+        Car car = new Car(290, "Chrysler PYR", 1974);
+        Assertions.assertEquals(car, CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withGoodDataWithModelFormat1_car() {
+        String stringCar1 = "Car{horsepower=290 hp, model=Chrysler PYR, year=1974}";
+        Car car = new Car(290, "Chrysler PYR", 1974);
+        Assertions.assertEquals(car, CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withGoodDataWithModelFormat2_car() {
+        String stringCar1 = "Car{horsepower=290 hp, model=\"Chrysler PYR\", year=1974}";
+        Car car = new Car(290, "Chrysler PYR", 1974);
+        Assertions.assertEquals(car, CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withGoodDataWithModelFormat3_car() {
+        String stringCar1 = "Car{horsepower=290 hp, model='Chrysler PYR\", year=1974}";
+        Car car = new Car(290, "Chrysler PYR", 1974);
+        Assertions.assertEquals(car, CarDeserializer.stringToCar(stringCar1));
+    }
+
+
+    @Test
+    public void stringToCar_withNull_null() {
+        String stringCar1 = null;
+        Assertions.assertNull(CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withEmpty_null() {
+        String stringCar1 = "";
+        Assertions.assertNull(CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_noKeys_null() {
+        String stringCar1 = "Car{290 hp, 'Chrysler PYR', 1974}";
+        Assertions.assertNull(CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withNoHorsepowerKey_null() {
+        String stringCar1 = "Car{model='Chrysler PYR', year=1974}";
+        Assertions.assertNull(CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withNoModelKey_null() {
+        String stringCar1 = "Car{horsepower=290 hp, year=1974}";
+        Assertions.assertNull(CarDeserializer.stringToCar(stringCar1));
+    }
+
+    @Test
+    public void stringToCar_withNoYearKey_null() {
+        String stringCar1 = "Car{horsepower=290 hp, model='Chrysler PYR'}";
+        Assertions.assertNull(CarDeserializer.stringToCar(stringCar1));
+    }
+}
---
+++ src/test/java/CarTest.java
@@ -0,0 +1,46 @@
+import org.example.Car;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Year;
+
+public class CarTest {
+
+    @Test
+    public void builder_build_withGoodData_car() {
+        Car expectedCar = new Car(123, "test", Year.now().getValue());
+        Car realCar = Car.builder().horsepower(123).model("test").year(Year.now().getValue()).build();
+        Assertions.assertEquals(expectedCar, realCar);
+    }
+
+
+    @Test
+    public void builder_build_withNegativeHorsepower_null() {
+        Car realCar = Car.builder().horsepower(-1).model("test").year(1990).build();
+        Assertions.assertNull(realCar);
+    }
+
+    @Test
+    public void builder_build_withNullModel_null() {
+        Car realCar = Car.builder().horsepower(123).model(null).year(1990).build();
+        Assertions.assertNull(realCar);
+    }
+
+    @Test
+    public void builder_build_withEmptyModel_null() {
+        Car realCar = Car.builder().horsepower(123).model("").year(1990).build();
+        Assertions.assertNull(realCar);
+    }
+
+    @Test
+    public void builder_build_withNegativeYear_null() {
+        Car realCar = Car.builder().horsepower(123).model("test").year(-1).build();
+        Assertions.assertNull(realCar);
+    }
+
+    @Test
+    public void builder_build_withOlderThanNowYear_null() {
+        Car realCar = Car.builder().horsepower(123).model("test").year(Year.now().getValue() + 1).build();
+        Assertions.assertNull(realCar);
+    }
+}
